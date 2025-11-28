1b32d472e59d39f5f6a84bd4e6f771f146966a63
admin <admin@DESKTOP-GRVFG8H>
2025-11-28 08:58:27 +0700

    pppp

--- rumahsakit_kel5/Kamar.Designer.cs
+++ rumahsakit_kel5/Kamar.Designer.cs
@@ -195,6 +195,7 @@
             this.pictureBox2.Size = new System.Drawing.Size(105, 65);
             this.pictureBox2.TabIndex = 12;
             this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // pictureBox1
             // 
